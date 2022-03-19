218f664926c4160066646b75b97f455ebfe9f7a0
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-19 01:32:37 -0600

    :bug: Loader Bug
    Loader Bug

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
     return (
-        <div className=" flex justify-center items-center p-5 " >
+        <div className=" w-full h-[90vh] flex justify-center items-center p-5 " >
             <span className=' w-40 h-40 border-8 border-l-purple-200 border-purple-500 animate-spin rounded-full ' >
 
             </span>
--- src/components/PokemonsList/PokemonList.jsx
+++ src/components/PokemonsList/PokemonList.jsx
@@ -10,7 +10,7 @@ const PokemonList = ({list}) => {
 
 
     return (
-       <section className=' w-full min-h-[100vh] flex flex-col items-center p-5 space-y-5 mt-[10vh] md:grid md:grid-cols-4 md:space-y-0  md:gap-10 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 '>
+       <section className=' w-full min-h-[0vh] flex flex-col items-center p-5 space-y-5 mt-[10vh] md:grid md:grid-cols-4 md:space-y-0  md:gap-10 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 '>
            {list.map((element , index) => {
                 return <PokemonCard key={`${index}-${element.species.name}`} pokemonInfo={element} />
            })}
