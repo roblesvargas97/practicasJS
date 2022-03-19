a877089d13383c219bfda181f53bc6dc4e0e822e
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-19 00:49:43 -0600

    :lipstick: Add Footer
    Add Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className=' w-full h-9 bg-red-500'>
-
+        <footer className=' w-full min-h-10 bg-red-500'>
+            <p className='text-xl text-center text-white font-bold' >Desarrollado por Jesus Uriel Robles Vargas</p>
+            <p className='text-center text-xl'>😅🤠😛</p>
         </footer>
     );
 }
