86615d519750510822f38ba6d4a9b12f207242d1
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-18 22:00:59 -0600

    :sparkles: FavsPokemon
    FavsPokemon

---
+++ src/components/PokemonFavItem/PokemonFavItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { GoTrashcan } from 'react-icons/go';
+
+const PokemonFavItem = ({pokemon}) => {
+
+    console.log(pokemon);
+
+    return (
+        <div className=" flex bg-purple-500 backdrop-blur rounded-md shadow-md shadow-slate-500 active:shadow-lg active:shadow-slate-500 " >
+            <div className=' flex justify-center items-center w-52 rounded-md bg-red-500 backdrop-blur' >
+                <img className='w-full' src={pokemon.sprites.front_default} alt="" />
+            </div>
+            <div className=' w-full flex flex-col  justify-center' >
+                <p className=' text-2xl first-letter:capitalize text-white font-bold text-center' >{pokemon.name}</p>
+                <button className=' self-center w-12  flex items-center justify-center bg-white rounded-md text-purple-500 p-2 active:scale-75 transition-all ' > <GoTrashcan size="30px" /> </button>
+            </div>
+        </div>
+    );
+}
+
+export default PokemonFavItem;
--- src/components/PokemonFavs/PokemonFavs.jsx
+++ src/components/PokemonFavs/PokemonFavs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import PokemonFavItem from '../PokemonFavItem/PokemonFavItem';
+import {FaSadCry} from 'react-icons/fa';
 
 const PokemonFavs = () => {
 
@@ -12,11 +13,18 @@ const PokemonFavs = () => {
     return (
         <section className={` fixed w-full h-[90vh] p-5 top-[10vh] bg-red-700/50 backdrop-blur  z-20 `}>
             <div className=' h-[80vh] bg-purple-500 rounded-md' >
-                <h2 className=' text-2xl font-bold text-center' >Favoritos</h2>
-                <div className=' h-full bg-white rounded-b-md ' >
-                    {favsPokemon.map(element => (
-                        <p>{element.name}</p>
-                    ))}
+                <h2 className=' text-2xl text-white font-bold text-center' >Favoritos</h2>
+                <div className=' h-full bg-white rounded-b-md space-y-5 overflow-y-scroll p-5 ' >
+                    {favsPokemon.length !== 0?
+                        favsPokemon.map(element => (
+                            <PokemonFavItem pokemon={element} />
+                        ))
+                        :
+                        <div className=' w-full bg-red-500 rounded-md flex flex-col items-center justify-center p-5 shadow-md text-white shadow-slate-500 active:shadow-lg active:shadow-slate-500 ' >
+                            <h3 className=' text-center text-2xl text-white font-bold' >Aun no has agregado ningun pokemon</h3>
+                            <FaSadCry size="60px" />
+                        </div>
+                    }
                 </div>
             </div>
         </section>
